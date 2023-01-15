88b4d569be311b971e8ba9d414a2bc30fd385f2f
aerobatic prop manipulator <89122797+safariragoli2@users.noreply.github.com>
2023-01-14 20:23:48 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -23,7 +23,7 @@ function self:createUI(title)
 	if game.CoreGui:FindFirstChild("customUiLibrary") then
 		game.CoreGui.customUiLibrary:Destroy()
 	end
-	
+
 	local newGui = Instance.new("ScreenGui", game.CoreGui)
 	local backgroundDim = Instance.new("Frame", newGui)
 	local frame = Instance.new("Frame", newGui)
@@ -37,21 +37,21 @@ function self:createUI(title)
 	Instance.new("UICorner", titleTab).CornerRadius = UDim.new(0, 4)
 	Instance.new("UICorner", tabsFrame).CornerRadius = UDim.new(0, 4)
 	Instance.new("UIListLayout", tabsFrame)
-	
+
 	newGui.Enabled = false
 	newGui.Name = "customUiLibrary"
 	newGui.IgnoreGuiInset = true
-	
+
 	backgroundDim.Size = UDim2.fromScale(1, 1)
 	backgroundDim.BorderSizePixel = 0
 	backgroundDim.BackgroundColor3 = Color3.new()
 	backgroundDim.BackgroundTransparency = 0.7
-	
+
 	frame.Position = UDim2.fromScale(0.5, 0.5)
 	frame.Size = UDim2.fromOffset(600, 300)
 	frame.BackgroundColor3 = self.activeColorSet.background
 	frame.ClipsDescendants = true
-	
+
 	titleTab.Size = UDim2.new(1, 0, 0, 15)
 	titleTab.TextXAlignment = Enum.TextXAlignment.Left
 	titleTab.TextSize = 14
@@ -59,12 +59,12 @@ function self:createUI(title)
 	titleTab.Text = "          "..title
 	titleTab.BackgroundColor3 = self.activeColorSet.titleTab
 	titleTab.TextColor3 = self.activeColorSet.textColor
-	
+
 	menuButton.Position = UDim2.fromOffset(2, 0)
 	menuButton.Size = UDim2.fromOffset(15, 15)
 	menuButton.BackgroundTransparency = 1
 	menuButton.Image = "rbxassetid://2738273138"
-	
+
 	tabsFrame.Visible = false	
 	tabsFrame.AnchorPoint = Vector2.new(0, 1)
 	tabsFrame.Position = UDim2.fromScale(0, 1)
@@ -72,34 +72,34 @@ function self:createUI(title)
 	tabsFrame.BackgroundColor3 = self.activeColorSet.tabsFrame
 	tabsFrame.BackgroundTransparency = 0.45
 	tabsFrame.ZIndex = 2
-	
+
 	contentArea.AnchorPoint = Vector2.new(0, 1)
 	contentArea.Position = UDim2.fromScale(0, 1)
 	contentArea.Size = UDim2.new(1, 0, 1, -15)
 	contentArea.BackgroundTransparency = 1
-	
+
 	resizeButton.AnchorPoint = Vector2.new(1, 1)
 	resizeButton.Size = UDim2.fromOffset(10, 10)
 	resizeButton.BorderSizePixel = 0
 	resizeButton.BackgroundColor3 = Color3.new()
 	resizeButton.Position = UDim2.fromScale(1, 1)
 	resizeButton.BackgroundTransparency = 0.8
-	
+
 	dragArea.BackgroundTransparency = 1
 	dragArea.Size = UDim2.fromScale(1, 1)
 	dragArea.ZIndex = 10
-	
+
 	pcall(function() getgenv().openUiConnection:Disconnect() end)
 	getgenv().openUiConnection = userInputService.InputBegan:Connect(function(key)
 		if key.KeyCode == self.openUiKey then
 			newGui.Enabled = not newGui.Enabled
 		end
 	end)
-	
+
 	menuButton.MouseButton1Click:Connect(function()
 		tabsFrame.Visible = not tabsFrame.Visible
 	end)
-	
+
 	self:enableResize(resizeButton, frame)
 	self:enableDrag(dragArea, frame)
 	self.mainUI = newGui
@@ -109,7 +109,7 @@ end
 
 function self:addNewTab(name, frame)
 	local tabButton = Instance.new("TextButton", self.tabsFrame)
-	
+
 	tabButton.Size = UDim2.new(1, 0, 0, 25)
 	tabButton.BackgroundTransparency = 1
 	tabButton.TextColor3 = self.activeColorSet.textColor
@@ -117,13 +117,18 @@ function self:addNewTab(name, frame)
 	tabButton.TextSize = 17
 	tabButton.FontFace = Font.new(tostring(Enum.Font.Ubuntu), Enum.FontWeight.Regular, Enum.FontStyle.Italic)
 	tabButton.ZIndex = 2
-	
+
 	tabButton.MouseButton1Click:Connect(function()
-		pcall(function()
-			if frame.Parent ~= self.contentArea then
-				self.contentArea:FindFirstChildOfClass("Frame").Parent = nil 
+		if frame.Parent ~= self.contentArea then
+			local frame = self.contentArea:FindFirstChildOfClass("Frame")
+			local scrollingFrame = self.contentArea:FindFirstChildOfClass("ScrollingFrame")
+			
+			if frame then
+				frame.Parent = nil
+			elseif scrollingFrame then
+				scrollingFrame.Parent = nil
 			end
-		end)
+		end
 		frame.Parent = (frame.Parent ~= self.contentArea) and self.contentArea or nil
 	end)
 end
@@ -166,18 +171,18 @@ function self:enableResize(frame, parent)
 	local mousePos
 	local frameSizeX
 	local frameSizeY
-	
+
 	parent = parent or frame
-	
+
 	frame.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 then
 			local startLocation = userInputService:GetMouseLocation()
-			
+
 			self.resizingFrame = true
 			mousePos = input.Position
 			frameSizeX = parent.Size.X.Offset
 			frameSizeY = parent.Size.Y.Offset
-			
+
 			spawn(function()
 				while self.resizingFrame do
 					task.wait()
@@ -187,12 +192,10 @@ function self:enableResize(frame, parent)
 			end)
 		end
 	end)
-	
+
 	frame.InputEnded:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 then
 			self.resizingFrame = false
 		end
 	end)
 end
-
-return self
